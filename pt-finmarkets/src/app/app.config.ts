cc5a60a8495abe45a1cc2663e351208dc2f27ca9
Benjamin <bsanzanasilva@gmail.com>
2025-09-11 13:34:48 -0300

    agreando assets, configuraciones ultimas

--- pt-finmarkets/src/app/app.config.ts
+++ pt-finmarkets/src/app/app.config.ts
@@ -1,21 +1,29 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { providePrimeNG } from 'primeng/config';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import Aura from '@primeuix/themes/aura';
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(withFetch()),
+    provideAnimationsAsync(),
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-     providePrimeNG({
-            theme: {
-                preset: Aura,
-                options: {
-                    darkModeSelector: 'system',
-                }
-            }
-        })
-  ]
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: 'system',
+        },
+      },
+    }),
+  ],
 };
